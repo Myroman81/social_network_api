55bfbcd95d8ca98764908158759eaef7b648ad97
myroman-wuLtCL <myroman@protonmail.com>
2023-05-15 19:18:31 -0400

    routes updated

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,11 +1,31 @@
+const userController = require('../../controllers/thoughtcontroller');
 
 const router = require('express').Router()
 
 // every route starts with '/api/thoughts'
-router.get('/', (req, res) => {
-    res.send("Hit Thoughts Route ...")
+
+
+// GET ALL THOUGHTS
+router.route('/', thoughtController.getAll);
+
+
+// GET SINGLE THOUGHTS
+router.get('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
 })
 
 
+// CREATE NEW THOUGHT
+router.create('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
 
+// EDIT EXISTING THOUGHT
+router.edit('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
+// DELETE THOUGHT
+router.delete('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
 module.exports = router;
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -16,10 +16,16 @@ router.get('/:id', (req, res) => {
 
 
 // CREATE NEW USER
-
+router.create('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
 
 // EDIT EXISTING USER by ID
-
+router.edit('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
 // DELETE USER by ID
-
+router.delete('/:id', (req, res) => {
+    console.log("ID: ", req.params.id);
+})
 module.exports = router;
